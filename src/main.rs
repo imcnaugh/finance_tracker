9717bf4d78a0d028d722e79298d0f7f1983d0651
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-08-05 21:43:28 -0500

    some basic framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Args, Parser};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Command::parse();
+    match cli {
+        Command::AddLineItem(line_item) => {
+            println!("Line item: {} - Quantity: {}, Price: ${:.2}", line_item.name, line_item.quantity, line_item.price);
+        }
+        Command::PrintReport => {
+            println!("Printing report");
+        }
+    }
+}
+
+#[derive(Args)]
+struct AddLineItem {
+    name: String,
+    quantity: f32,
+    price: f32,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+enum Command {
+    #[command(about = "Add a line item to the report")]
+    AddLineItem(AddLineItem),
+    #[command(about = "Print the report")]
+    PrintReport,
 }
