6d5232e2ff87aa2e9789f18dabdceaae129c8482
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-08-09 22:28:06 -0400

    create new client struct for client creating

---
+++ src/model/new_client.rs
@@ -0,0 +1,13 @@
+pub struct NewClient {
+    name: String,
+}
+
+impl NewClient {
+    pub(crate) fn new(name: String) -> Self {
+        Self { name }
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+}
