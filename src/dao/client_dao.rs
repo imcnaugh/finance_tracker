7b19457ec0acc6296f5273674183ff7007798751
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-08-17 17:27:54 -0500

    already better i think

--- src/dao/client_dao.rs
+++ src/dao/client_dao.rs
@@ -1,3 +1,8 @@
+use crate::dao::error::DatabaseError;
+use crate::model::NewClient;
+use crate::model::client::Client;
+
 pub trait ClientDao {
-    fn create_client();
+    fn create_client(new_client: NewClient) -> Result<Client, DatabaseError>;
+    fn get_client_by_id(id: &str) -> Result<Option<Client>, DatabaseError>;
 }
--- src/dao/mod.rs
+++ src/dao/mod.rs
@@ -1,4 +1,5 @@
 pub mod client_dao;
+mod error;
 pub mod invoice_dao;
 pub mod line_item_dao;
 // pub mod sqlite;
