a913217afd2f39ac39d43f451fd765c8b09afe7a
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-08-24 12:49:20 -0500

    turningin on foreign key enforcement

--- src/dao/sqlite/sqlite_connection.rs
+++ src/dao/sqlite/sqlite_connection.rs
@@ -29,6 +29,11 @@ pub async fn get_pooled_connection() -> Result<PoolConnection<Sqlite>, sqlx::Err
                 .await
                 .expect("Failed to connect to database");
 
+            sqlx::query("PRAGMA foreign_keys = ON;")
+                .execute(&connection_pool)
+                .await
+                .expect("Failed to enable foreign keys");
+
             sqlx::migrate!("./db/migrations")
                 .run(&connection_pool)
                 .await
