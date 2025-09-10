fd2d3ca3e7b66698d6962ccded5f9d7ecd1aa12f
Ian Mcnaughton <IanMcNaugh@gmail.com>
2025-09-09 22:16:20 -0500

    cleaning up some printlns

--- invoice_manager/src/service/config_service.rs
+++ invoice_manager/src/service/config_service.rs
@@ -8,17 +8,14 @@ use std::path::PathBuf;
 pub fn get_config() -> Result<Configuration, String> {
     let path = get_config_path().unwrap();
 
-    println!("Config path: {:?}", path);
-
     if !path.exists() {
-        println!("Config file does not exist at path: {:?}", path);
         return Err("Config file does not exist".to_string());
     }
 
     let content =
         fs::read_to_string(path).map_err(|e| format!("Failed to read config file: {}", e))?;
 
-    toml::from_str(&content).map_err(|e| format!("Failed to parse config file: {}", e))?
+    toml::from_str(&content).map_err(|e| format!("Failed to parse config file: {}", e))
 }
 
 pub fn create_config(init_config: NewCompanyConfiguration) -> Result<(), String> {
@@ -44,11 +41,9 @@ pub fn create_config(init_config: NewCompanyConfiguration) -> Result<(), String>
 fn get_config_path() -> Option<PathBuf> {
     #[cfg(feature = "dev")]
     {
-        println!("Using dev config");
         return Some(PathBuf::from("config.dev.toml"));
     }
 
-    println!("Using prod config");
     let path = ProjectDirs::from("com", "caffeine-driven-development", "finance-app");
     path.map(|p| p.config_dir().join("config.toml"))
 }
