d15f204389613076fa5e78cb65f8cac2b0f63188
Ian McNaughton <IanMcNaugh@gmail.com>
2025-09-29 20:58:15 -0500

    I like these names better, now i need to fix up the models

--- double_entry_bookkeeping/db/migrations/1_initialSql.sql
+++ double_entry_bookkeeping/db/migrations/1_initialSql.sql
@@ -12,23 +12,23 @@ CREATE TABLE account (
     FOREIGN KEY (account_type_id) REFERENCES account_type(id)
 );
 
-CREATE TABLE journal (
+CREATE TABLE journal_entry(
      id INTEGER PRIMARY KEY AUTOINCREMENT,
      description TEXT NOT NULL,
      created_timestamp INT NOT NULL DEFAULT (unixepoch())
 );
 
-CREATE TABLE ledger (
+CREATE TABLE transaction(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     account_id INTEGER NOT NULL,
-    journal_id INTEGER NOT NULL,
+    journal_entry_id INTEGER NOT NULL,
     amount_in_cents INT NOT NULL,
     is_debit BOOLEAN NOT NULL,
     created_timestamp INT NOT NULL DEFAULT (unixepoch()),
     FOREIGN KEY (account_id) REFERENCES account(id),
-    FOREIGN KEY (journal_id) REFERENCES journal(id)
+    FOREIGN KEY (journal_entry_id) REFERENCES journal_entry(id)
 );
 
 CREATE INDEX idx_account_type ON account (account_type_id);
-CREATE INDEX idx_ledger_account_id ON ledger (account_id);
-CREATE INDEX idx_ledger_journal_id ON ledger (journal_id);
+CREATE INDEX idx_transaction_account_id ON transaction (account_id);
+CREATE INDEX idx_transaction_journal_entry_id ON transaction (journal_entry_id);
