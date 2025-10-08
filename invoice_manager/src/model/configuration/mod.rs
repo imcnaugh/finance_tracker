52eb359b8916d2a9e4d3520ffb175118a4d97697
Ian McNaughton <IanMcNaugh@gmail.com>
2025-10-07 23:45:06 -0500

    cargo check fix

--- invoice_manager/src/model/configuration/mod.rs
+++ invoice_manager/src/model/configuration/mod.rs
@@ -2,5 +2,4 @@ mod company_configuration;
 pub mod new_company_configuration;
 
 pub use company_configuration::CompanyConfiguration;
-use serde::{Deserialize, Serialize};
 pub use utilities::database_configuration::DatabaseConfiguration;
